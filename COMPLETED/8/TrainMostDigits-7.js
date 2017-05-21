a3d4a60e9fcf5673dba386aeacd8f0f71049ee0b
Maddie Humphreys <heymaddieh@users.noreply.github.com>
2017-05-21 15:44:38 -0600

    new level 7 kata

---
+++ COMPLETED/8/TrainMostDigits-7.js
@@ -0,0 +1,25 @@
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+function findLongest(array){
+  var highest = array[0];
+  var highestInt = array[0].toString().length
+  for(var i=0; i<array.length; i++){
+    var now = array[i].toString().length;
+    
+    for(var j=1; j<array.length; j++){
+      var next = array[j].toString().length;
+      
+      if(highest === 0){
+          highestInt = now
+          highest = array[i];
+          
+      } else if(next > highestInt){
+          highest = array[j];
+          highestInt = next
+      }
+    }
+  }
+  return highest;
+}
