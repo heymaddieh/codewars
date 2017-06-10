f6e80496728676aa08e57790a50dc50832f54f0d
Maddie Humphreys <heymaddieh@users.noreply.github.com>
2017-06-10 17:19:49 -0600

    finished Decipher this

--- InProgress/DecipherThis-6.js
+++ InProgress/DecipherThis-6.js
@@ -13,16 +13,25 @@
 // decipherThis('82yade 115te 103o'); // 'Ready set go'
 
 function decipherThis(str) {
-  var newStr;
-  var splitStr = str.split(' ');
-  console.log(splitStr);
-
-  for(var i = 0; i<splitStr.length; i++){
-    var number = splitStr[i].match(/\d+/g).map(Number);
-      console.log(number)
-    var letter = String.fromCharCode(number)
-      console.log(letter)
+  var newStr = [];
+  var splitStr = str.split(' ');/* Split string into an array of the words in the string */
+  for(var i = 0; i<splitStr.length; i++){ /*Loop through the splitStr array to analyze each word individually.*/
+    var number = splitStr[i].match(/\d+/g).map(Number)/* Singles out the number in the string */
+    var letter = String.fromCharCode(number) /* Converts the number into a letter of the alphabet */
+    var letters = []
+    var splitWord = splitStr[i].split('')
+    
+    for(var j = 0; j<splitWord.length; j++){
+      if(splitWord[j] >= 0 && splitWord[j] <= 9){
+      } else {
+        letters.push(splitWord[j])
+      }
+    }
+    letters[0] = [letters[letters.length-1],letters[letters.length-1]=letters[0]][0]
+    letters.unshift(letter)
+    newStr.push(letters.join(''))
   }
+  return newStr.join(' ')
 }; 
 
 decipherThis('72olle 103doo 100ya');
